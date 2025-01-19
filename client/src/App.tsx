5d7a85fa1774ead39ef71b77e7abbc54784a8d85
MelatoninJr <dillon32300@gmail.com>
2025-01-19 02:31:01 -0600

    Force-added client folder and contents

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import LandingPage from './views/Home'
+import CustomNavbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <CustomNavbar />
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
